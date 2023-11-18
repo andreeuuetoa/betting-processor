3cb341d66f7541c1f8ab942e4ff7e62c1ce0b81e
andreeuuetoa <andree.uuetoa@gmail.com>
2023-11-18 23:06:29 +0200

    Have match outcome parser take in the outcome as string instead of an array

--- src/main/java/util/files/MatchDataFileReader.java
+++ src/main/java/util/files/MatchDataFileReader.java
@@ -34,16 +34,16 @@ public class MatchDataFileReader {
         UUID matchId = UUID.fromString(matchElements[0]);
         double aBetRate = Double.parseDouble(matchElements[1]);
         double bBetRate = Double.parseDouble(matchElements[2]);
-        MatchOutcome matchOutcome = parseMatchOutcomeFromString(matchElements);
+        MatchOutcome matchOutcome = parseMatchOutcomeFromString(matchElements[3]);
         return new Match(matchId, new MatchData(aBetRate, bBetRate, matchOutcome));
     }
 
-    private static MatchOutcome parseMatchOutcomeFromString(String[] matchElements) {
-        return switch (matchElements[3]) {
+    private static MatchOutcome parseMatchOutcomeFromString(String matchOutcomeAsString) {
+        return switch (matchOutcomeAsString) {
             case "A" -> MatchOutcome.A;
             case "B" -> MatchOutcome.B;
             case "DRAW" -> MatchOutcome.DRAW;
-            default -> throw new IllegalStateException("Unexpected match outcome: " + matchElements[3]);
+            default -> throw new IllegalStateException("Unexpected match outcome: " + matchOutcomeAsString);
         };
     }
 }
