fc1f4e73d5c78d1bcce4beb382769785188350aa
Andree Uuetoa <andree.uuetoa@gmail.com>
2023-11-19 13:57:12 +0200

    Generate the output data of multiple legitimate players

--- src/main/java/util/outputdata/ResultGenerator.java
+++ src/main/java/util/outputdata/ResultGenerator.java
@@ -3,6 +3,8 @@ package util.outputdata;
 import dto.PlayerAction;
 import dto.ResultLegitimatePlayer;
 
+import java.util.List;
+
 public class ResultGenerator {
 	public String generateLegitimatePlayerResult(ResultLegitimatePlayer legitimatePlayer) {
 		return String.format("%s %d %.2f",
@@ -18,5 +20,22 @@ public class ResultGenerator {
 				illegitimatePlayerAction.getMatchId(),
 				illegitimatePlayerAction.getAmount(),
 				illegitimatePlayerAction.getBettingSide());
+	}
+
+	public String generateLegitimatePlayersResult(List<ResultLegitimatePlayer> legitimatePlayers) {
+		int playerCount = 0;
+		StringBuilder legitimatePlayersStringBuilder = new StringBuilder();
+		for (ResultLegitimatePlayer legitimatePlayer : legitimatePlayers) {
+			legitimatePlayersStringBuilder.append(generateLegitimatePlayerResult(legitimatePlayer));
+			if (isLastPlayerInList(legitimatePlayers, playerCount)) {
+				legitimatePlayersStringBuilder.append("\n");
+			}
+			playerCount++;
+		}
+		return legitimatePlayersStringBuilder.toString();
+	}
+
+	private boolean isLastPlayerInList(List<ResultLegitimatePlayer> legitimatePlayers, int indexOfList) {
+		return indexOfList < legitimatePlayers.size() - 1;
 	}
 }
--- src/test/java/util/outputdata/ResultGeneratorTest.java
+++ src/test/java/util/outputdata/ResultGeneratorTest.java
@@ -6,6 +6,7 @@ import dto.PlayerAction;
 import dto.ResultLegitimatePlayer;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +21,22 @@ public class ResultGeneratorTest {
 		ResultLegitimatePlayer legitimatePlayer = new ResultLegitimatePlayer(playerId, finalBalance, bettingWinRate);
 		String actualLegitimatePlayerAsString = new ResultGenerator().generateLegitimatePlayerResult(legitimatePlayer);
 		assertEquals(expectedLegitimatePlayerAsString, actualLegitimatePlayerAsString);
+	}
+
+	@Test
+	public void testMultipleLegitimatePlayersToString() {
+		String expectedLegitimatePlayersAsString = "163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0,80\nabae2255-4255-4304-8589-737cdff61640 5000 0,95\n5de45517-4312-4596-9702-9277b6d649f2 918 0,45";
+		List<ResultLegitimatePlayer> threeSampleLegitimatePlayers = generateThreeSampleLegitimatePlayers();
+		String actualLegitimatePlayersAsString = new ResultGenerator().generateLegitimatePlayersResult(threeSampleLegitimatePlayers);
+		assertEquals(expectedLegitimatePlayersAsString, actualLegitimatePlayersAsString);
+	}
+
+	private List<ResultLegitimatePlayer> generateThreeSampleLegitimatePlayers() {
+		return List.of(
+				new ResultLegitimatePlayer(UUID.fromString("163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4"), 4321, 0.8),
+				new ResultLegitimatePlayer(UUID.fromString("abae2255-4255-4304-8589-737cdff61640"), 5000, 0.95),
+				new ResultLegitimatePlayer(UUID.fromString("5de45517-4312-4596-9702-9277b6d649f2"), 918, 0.45)
+		);
 	}
 
 	@Test
