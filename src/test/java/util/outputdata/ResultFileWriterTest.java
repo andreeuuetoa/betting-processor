d342094da3866f0c5c57bdeb623a5fc3d14dfa53
Andree Uuetoa <andree.uuetoa@gmail.com>
2023-11-19 14:48:54 +0200

    ResultFileWriter can write multi-line data to file

--- src/test/java/util/outputdata/ResultFileWriterTest.java
+++ src/test/java/util/outputdata/ResultFileWriterTest.java
@@ -19,8 +19,33 @@ public class ResultFileWriterTest {
 		new ResultFileWriter(path).writeResultToFile(sampleData);
 		assertDoesNotThrow(() -> {Files.newBufferedReader(path);});
 		try (BufferedReader reader = Files.newBufferedReader(path)) {
-			String line = reader.readLine();
-			assertEquals(sampleData, line);
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) break;
+				assertEquals(sampleData, line);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void testWriteSampleDataWithNewLinesToResultFile() {
+		Path path = Paths.get("src", "test", "resources", "result", "test_file.txt");
+		String sampleData = "Hello, world!\nNew line!";
+		new ResultFileWriter(path).writeResultToFile(sampleData);
+		assertDoesNotThrow(() -> {Files.newBufferedReader(path);});
+		try (BufferedReader reader = Files.newBufferedReader(path)) {
+			StringBuilder dataInFile = new StringBuilder();
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) break;
+				if (!dataInFile.isEmpty()) {
+					dataInFile.append("\n");
+				}
+				dataInFile.append(line);
+			}
+			assertEquals(sampleData, dataInFile.toString());
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
