f60631d4b244ae0282203a945b9a651e7e3726cf
andreeuuetoa <andree.uuetoa@gmail.com>
2023-11-19 22:09:54 +0200

    Make BettingProcessor happy path test actually meaningful

--- src/test/java/bettingprocessor/BettingProcessorTest.java
+++ src/test/java/bettingprocessor/BettingProcessorTest.java
@@ -2,11 +2,14 @@ package bettingprocessor;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BettingProcessorTest {
 	@Test
@@ -16,6 +19,12 @@ public class BettingProcessorTest {
 		Path resultPath = Paths.get("src", "test", "resources", "result", "results.txt");
 		BettingProcessor bettingProcessor = new BettingProcessor(matchDataPath, playerDataPath, resultPath);
 		bettingProcessor.processBettingData();
-		assertDoesNotThrow(() -> Files.newBufferedReader(resultPath));
-	}
+		try (BufferedReader reader = Files.newBufferedReader(resultPath)) {
+			for (int i = 0; i < 5; i++) {
+				assertNotNull(reader.readLine());
+			}
+		} catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
