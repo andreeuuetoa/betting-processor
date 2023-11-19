1b4f5bf41cf4165695b57f78ee92673c49182f2d
Andree Uuetoa <andree.uuetoa@gmail.com>
2023-11-19 13:04:12 +0200

    Remove redundant testing of match parsing

--- src/test/java/util/files/MatchDataFileExtractorTest.java
+++ src/test/java/util/files/MatchDataFileExtractorTest.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static domain.constants.MatchOutcome.A;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MatchDataFileExtractorTest {
@@ -16,15 +15,6 @@ public class MatchDataFileExtractorTest {
 		Path path = Paths.get("src", "test", "resources", "matchdata", "one_match_data.txt");
 		List<MatchInfo> matchesFromFile = new MatchDataFileExtractor().extractMatchInfoFromFileInPath(path);
 		assertEquals(1, matchesFromFile.size());
-		MatchInfo parsedMatchInfo = matchesFromFile.get(0);
-		assertMatchInfoWasParsedCorrectly(parsedMatchInfo);
-	}
-
-	private void assertMatchInfoWasParsedCorrectly(MatchInfo parsedMatchInfo) {
-		assertEquals("abae2255-4255-4304-8589-737cdff61640", parsedMatchInfo.getId().toString());
-		assertEquals(1.45, parsedMatchInfo.getMatchData().getABetRate());
-		assertEquals(0.75, parsedMatchInfo.getMatchData().getBBetRate());
-		assertEquals(A, parsedMatchInfo.getMatchData().getMatchOutcome());
 	}
 
 	@Test
