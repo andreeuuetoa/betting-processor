4ec5994128b8488dc4a803ba2de6ab65070d1166
andreeuuetoa <andree.uuetoa@gmail.com>
2023-11-19 22:04:05 +0200

    Fix tests

--- src/test/java/util/outputdata/ResultGeneratorTest.java
+++ src/test/java/util/outputdata/ResultGeneratorTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ResultGeneratorTest {
 	@Test
 	public void testLegitimatePlayerToString() {
-		String expectedLegitimatePlayerAsString = "163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0,80";
+		String expectedLegitimatePlayerAsString = "163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0.80";
 		UUID playerId = UUID.fromString("163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4");
 		int finalBalance = 4321;
 		double bettingWinRate = 0.8;
@@ -25,7 +25,7 @@ public class ResultGeneratorTest {
 
 	@Test
 	public void testMultipleLegitimatePlayersToString() {
-		String expectedLegitimatePlayersAsString = "163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0,80\nabae2255-4255-4304-8589-737cdff61640 5000 0,95\n5de45517-4312-4596-9702-9277b6d649f2 918 0,45";
+		String expectedLegitimatePlayersAsString = "163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0.80\nabae2255-4255-4304-8589-737cdff61640 5000 0.95\n5de45517-4312-4596-9702-9277b6d649f2 918 0.45";
 		List<ResultLegitimatePlayer> threeSampleLegitimatePlayers = generateThreeSampleLegitimatePlayers();
 		String actualLegitimatePlayersAsString = new ResultGenerator().generateLegitimatePlayersResult(threeSampleLegitimatePlayers);
 		assertEquals(expectedLegitimatePlayersAsString, actualLegitimatePlayersAsString);
@@ -86,9 +86,9 @@ public class ResultGeneratorTest {
 	@Test
 	public void testGenerateFullResultAsString() {
 		String expectedResultString = """
-				163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0,80
-				abae2255-4255-4304-8589-737cdff61640 5000 0,95
-				5de45517-4312-4596-9702-9277b6d649f2 918 0,45
+				163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 4321 0.80
+				abae2255-4255-4304-8589-737cdff61640 5000 0.95
+				5de45517-4312-4596-9702-9277b6d649f2 918 0.45
 				
 				163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 BET abae2255-4255-4304-8589-737cdff61640 5000 A
 				4925ac98-833b-454b-9342-13ed3dfd3ccf WITHDRAW null 8093 null
